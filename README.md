0b3bf67bd91ed03f6a763609ef2179d7d41be57e
frankdb <fd.barros@yahoo.com>
2019-03-03 14:32:47 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This was built as a project for San Diego Code School. Its purpose was to help me become familiar with Gulp which is a toolkit for automating tasks in development.
+
+To load this project, clone this repository to your computer. Then open up the terminal, navigate to the directory where you cloned the project and run "gulp". This will run the gulp tasks and start the server. You can see the finished web page on http://localhost:8080.
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,44 @@
 // write your code here
 
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var nodemon = require("gulp-nodemon");
+
+gulp.task("build:js", function() {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(concat("bundle.min.js"))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("build:css", function() {
+  return gulp
+    .src("src/css/*.css")
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src("src/assets/*.png").pipe(gulp.dest("dist/assets"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./src/css*.css", ["build:css"]);
+  gulp.watch("./src/js/*.js", ["build:js"]);
+  gulp.watch("./src/assets/*.png", ["copy"]);
+});
+
+gulp.task("serve", function() {
+  return nodemon({
+    script: "server/index.js",
+    env: {
+      NODE_ENV: "development"
+    }
+  });
+});
+
+gulp.task("default", ["build:js", "build:css", "copy", "watch", "serve"]);
+
 module.exports = gulp;
